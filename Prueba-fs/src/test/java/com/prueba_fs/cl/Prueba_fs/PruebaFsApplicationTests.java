3cf1b42e78583ae087f6f14cd9e71bd3451bba27
Nicola Osses <165812260+itzAragorn@users.noreply.github.com>
2025-05-12 09:25:54 -0400

    Primera carga (base)
    Archivo base de spring boot

---
+++ Prueba-fs/src/test/java/com/prueba_fs/cl/Prueba_fs/PruebaFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_fs.cl.Prueba_fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
