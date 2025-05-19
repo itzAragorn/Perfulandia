79f7782b185b11c3a66773a8d71e9bee36191f96
Brunowo <br.arayap@duocuc.cl>
2025-05-19 13:25:05 -0400

    Delete Prueba-fs directory

--- Prueba-fs/src/main/java/com/prueba_fs/cl/Prueba_fs/repository/EmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prueba_fs.cl.Prueba_fs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prueba_fs.cl.Prueba_fs.model.Empleado;
-
-public interface EmpleadoRepository extends JpaRepository<Empleado, Long>{
-
-}
