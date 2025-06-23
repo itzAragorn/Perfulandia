e9531be2f434d9c6eb55244d0d373f09174b0b36
Nicola Osses <165812260+itzAragorn@users.noreply.github.com>
2025-06-23 17:33:18 -0400

    Delete Prueba-fs directory

--- Prueba-fs/src/main/java/com/prueba_fs/cl/Prueba_fs/repository/AdministradorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prueba_fs.cl.Prueba_fs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prueba_fs.cl.Prueba_fs.model.Administrador;
-
-public interface AdministradorRepository extends JpaRepository<Administrador, Long>{
-    
-}
